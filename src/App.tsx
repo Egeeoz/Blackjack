19b4895a75990b7d360d73abb64df8377efd915c
Egeeoz <ozakdemirege@hotmail.com>
2024-07-04 22:37:43 +0200

    Refactor project and code

--- src/App.tsx
+++ src/App.tsx
@@ -1,121 +1,36 @@
-import { useState } from 'react';
 import './App.css';
-import {
-  createDeck,
-  shuffleDeck,
-  dealCard,
-  Card,
-  calculatePoints,
-} from './blackjackLogic';
+import { calculatePoints } from './blackjackLogic';
 
-// TODO when player busts make so player cant press stay
+import gameLogic from './gameLogic';
+
+// TODO Decide winner and loser
 
 function App() {
-  const [deck, setDeck] = useState<Card[]>(shuffleDeck(createDeck()));
-  const [playerHand, setPlayerHand] = useState<Card[]>([]);
-  const [dealerHand, setDealerHand] = useState<Card[]>([]);
-  const [activeGame, setActiveGame] = useState<boolean>(false);
-  const [playerStayed, setPlayerStayed] = useState<Boolean>(false);
-  const [playerPoints, setPlayerPoints] = useState<number>(0);
-  const [dealerPoints, setDealerPoints] = useState<number>(0);
-  const [playerChips, setPlayerChips] = useState<number>(500);
-  const [playerBet, setPlayerBet] = useState<number>(0);
+  const {
+    handleBet,
+    handleDealCard,
+    handleHit,
+    handleStay,
+    handleNewGame,
+    playerHand,
+    dealerHand,
+    activeGame,
+    playerStayed,
+    playerPoints,
+    dealerPoints,
+    playerChips,
+    playerBet,
+  } = gameLogic();
 
-  const handleBet = () => {
+  // Handle the button to place bet
+  const handlePlaceBet = () => {
     const betInput = document.getElementById(
       'betInput'
     ) as HTMLInputElement | null;
     if (betInput) {
-      let betAmount = parseInt(betInput.value, 10);
-      if (!isNaN(betAmount)) {
-        setPlayerBet(betAmount);
-        setPlayerChips(playerChips - betAmount);
-        console.log(playerChips);
-      }
+      handleBet(betInput.value);
+      betInput.value = '';
     }
-  };
-
-  const handleDealCard = () => {
-    if (activeGame || playerStayed) return;
-
-    const newPlayerCards = [];
-    const newDealerCards = [];
-
-    for (let i = 0; i < 4; i++) {
-      const card = dealCard(deck);
-      if (card) {
-        if (i % 2 === 0) {
-          newPlayerCards.push(card);
-        } else {
-          newDealerCards.push(card);
-        }
-      }
-    }
-
-    if (newPlayerCards.length > 0 || newDealerCards.length > 0) {
-      const updatedPlayerHand = [...playerHand, ...newPlayerCards];
-      const updatedDealerHand = [...dealerHand, ...newDealerCards];
-
-      setPlayerHand([...playerHand, ...newPlayerCards]);
-      setDealerHand([...dealerHand, ...newDealerCards]);
-      setDeck(deck);
-      setPlayerPoints(calculatePoints(updatedPlayerHand));
-      setDealerPoints(calculatePoints(updatedDealerHand));
-      setActiveGame(true);
-    }
-  };
-
-  const handleHit = () => {
-    if (!activeGame) return;
-
-    const card = dealCard(deck);
-    if (card) {
-      const updatedPlayerHand = [...playerHand, card];
-      setPlayerHand([...playerHand, card]);
-      setDeck(deck);
-      const newPlayerPoints = calculatePoints(updatedPlayerHand);
-      setPlayerPoints(newPlayerPoints);
-
-      if (newPlayerPoints > 21) {
-        setActiveGame(false);
-        setPlayerStayed(true);
-      }
-    }
-  };
-
-  const handleStay = () => {
-    if (!activeGame) return;
-
-    let updatedDealerHand = [...dealerHand];
-
-    while (calculatePoints(updatedDealerHand) <= 16) {
-      const card = dealCard(deck);
-      if (card) {
-        updatedDealerHand.push(card);
-      }
-    }
-
-    setDealerHand(updatedDealerHand);
-    setDeck(deck);
-    const newDealerPoints = calculatePoints(updatedDealerHand);
-    setDealerPoints(newDealerPoints);
-
-    if (newDealerPoints > 21) {
-      setActiveGame(false);
-    }
-
-    setPlayerStayed(true);
-  };
-
-  const handleNewGame = () => {
-    const newDeck = shuffleDeck(createDeck());
-    setDeck(newDeck);
-    setPlayerHand([]);
-    setDealerHand([]);
-    setActiveGame(false);
-    setPlayerStayed(false);
-    setPlayerPoints(0);
-    setDealerPoints(0);
   };
 
   return (
@@ -148,7 +63,10 @@ function App() {
       <button onClick={handleHit}>Hit</button>
       <button onClick={handleStay}>Stay</button>
       <input type="text" id="betInput" placeholder="Bet amount" />
-      <button onClick={handleBet}>Place Bet</button>
+      <p>
+        total bet: {playerBet} <br /> player chips: {playerChips}
+      </p>
+      <button onClick={handlePlaceBet}>Place Bet</button>
     </div>
   );
 }
---
+++ src/gameLogic.ts
@@ -0,0 +1,162 @@
+import { useState } from 'react';
+import {
+  createDeck,
+  shuffleDeck,
+  dealCard,
+  Card,
+  calculatePoints,
+} from './blackjackLogic';
+
+const gameLogic = () => {
+  const [deck, setDeck] = useState<Card[]>(shuffleDeck(createDeck()));
+  const [playerHand, setPlayerHand] = useState<Card[]>([]);
+  const [dealerHand, setDealerHand] = useState<Card[]>([]);
+  const [activeGame, setActiveGame] = useState<boolean>(false);
+  const [playerStayed, setPlayerStayed] = useState<Boolean>(false);
+  const [playerPoints, setPlayerPoints] = useState<number>(0);
+  const [dealerPoints, setDealerPoints] = useState<number>(0);
+  const [playerChips, setPlayerChips] = useState<number>(500);
+  const [playerBet, setPlayerBet] = useState<number>(0);
+
+  // Function to handle player bet
+  const handleBet = (betInput: string) => {
+    // If there is a bet the parse it and pass it into the state
+    if (betInput) {
+      let betAmount = parseInt(betInput, 10);
+      if (!isNaN(betAmount)) {
+        setPlayerBet(betAmount);
+        setPlayerChips(playerChips - betAmount);
+      }
+    }
+  };
+
+  //   Function to deal cards to player and dealer
+  const handleDealCard = () => {
+    if (!playerBet || activeGame || playerStayed) return;
+
+    const newPlayerCards = [];
+    const newDealerCards = [];
+
+    // Dealing cards
+    for (let i = 0; i < 4; i++) {
+      const card = dealCard(deck);
+      if (i % 2 === 0) {
+        newPlayerCards.push(card);
+      } else {
+        newDealerCards.push(card);
+      }
+    }
+
+    // Check if they are any new cards dealt
+    if (newPlayerCards.length > 0 || newDealerCards.length > 0) {
+      // Ensures that valid cards are kept and nothing is undefined in the array
+      const filteredPlayerCards = newPlayerCards.filter(
+        (card): card is Card => card !== undefined
+      );
+      const filteredDealerCards = newDealerCards.filter(
+        (card): card is Card => card !== undefined
+      );
+
+      //   Update and set dealer and player hand to the updated hand
+      const updatedPlayerHand = [...playerHand, ...filteredPlayerCards];
+      const updateDealerHand = [...dealerHand, ...filteredDealerCards];
+      setPlayerHand(updatedPlayerHand);
+      setDealerHand(updateDealerHand);
+
+      //   Calculate and update player and dealer points (total card values)
+      setPlayerPoints(calculatePoints(updatedPlayerHand));
+      setPlayerPoints(calculatePoints(updateDealerHand));
+
+      //   Set game to active so cards cant be dealt again
+      setActiveGame(true);
+    }
+  };
+
+  //   Function to give a player a card when pressed on corresponding button
+  const handleHit = () => {
+    if (!activeGame || playerStayed) return;
+
+    // take a card out from the deck
+    const card = dealCard(deck);
+
+    // If card exists update the player hand with the latest card
+    if (card) {
+      const updatedPlayerHand = [...playerHand, card];
+
+      setPlayerHand(updatedPlayerHand);
+
+      // Update the deck
+      setDeck(deck);
+
+      //   Calculate the players new points and update it
+      const newPlayerPoints = calculatePoints(updatedPlayerHand);
+      setPlayerPoints(newPlayerPoints);
+
+      //  TODO Check if player bust? maybe somewhere else
+      //   if (newPlayerPoints > 21) {
+      //     setActiveGame(false);
+      //     setPlayerStayed(true);
+      //   }
+    }
+  };
+  //   Function to handle player stay
+  const handleStay = () => {
+    if (!activeGame) return;
+
+    // Create a copy dealers hand and a copy of the deck
+    let updatedDealerHand = [...dealerHand];
+    let updatedDeck = [...deck];
+
+    // If the dealer has less than 16 points keep adding cards
+    while (calculatePoints(updatedDealerHand) <= 16) {
+      const card = dealCard(updatedDeck);
+      if (card) {
+        updatedDealerHand.push(card); // Update the dealers copied hand
+      }
+    }
+
+    // Update the state with the new hand
+    setDealerHand(updatedDealerHand);
+
+    // Update the deck state with the updated deck
+    setDeck(updatedDeck);
+
+    // Calculate dealers new hand update the points state
+    const newDealerPoints = calculatePoints(updatedDealerHand);
+    setDealerPoints(newDealerPoints);
+
+    setPlayerStayed(true);
+  };
+
+  //   Reset the game
+  const handleNewGame = () => {
+    const newDeck = shuffleDeck(createDeck());
+    setDeck(newDeck);
+    setPlayerHand([]);
+    setDealerHand([]);
+    setActiveGame(false);
+    setPlayerStayed(false);
+    setPlayerPoints(0);
+    setDealerPoints(0);
+    setPlayerChips(500);
+    setPlayerBet(0);
+  };
+
+  return {
+    handleDealCard,
+    handleBet,
+    handleHit,
+    handleStay,
+    handleNewGame,
+    playerHand,
+    dealerHand,
+    activeGame,
+    playerStayed,
+    playerPoints,
+    dealerPoints,
+    playerChips,
+    playerBet,
+  };
+};
+
+export default gameLogic;
